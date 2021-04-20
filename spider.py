588f40ae7c0e2e35f17b0b96d2299b03200eeb56
chen2 <2397651454@qq.com>
2021-04-20 23:00:01 +0800

    plus

--- spider.py
+++ spider.py
@@ -1,3 +1,5 @@
+import csv
+
 import requests
 import json
 
@@ -15,10 +17,15 @@ def main():
     base = "https://v0.yiketianqi.com/api?version=history&appid=79832935&appsecret=2qocDt9D&city="
     date = "&year=2021&month=3"
     cities = ['杭州','宁波','温州','嘉兴','湖州','绍兴','金华','衢州','舟山','台州','丽水']
-    for city in cities:
-        url = str(base+city+date)
-        data=getHTMLText(url)
-        print(data)
+    with open('data.csv', 'w+') as file:
+        writer = csv.writer(file)
+        writer.writerow(['city','ymd','bWendu','yWendu','tianqi','fengxiang','fengli','aqi','aqiInfo','aqiLevel'])
+        for city in cities:
+            url = str(base+city+date)
+            data = json.loads(getHTMLText(url))
+            for i in range(0, len(data['data'])):
+                writer.writerow([data['city'],data['data'][i]['ymd'],data['data'][i]['bWendu'],data['data'][i]['yWendu'],data['data'][i]['tianqi'],data['data'][i]['fengxiang'],data['data'][i]['fengli'],data['data'][i]['aqi'],data['data'][i]['aqiInfo'],data['data'][i]['aqiLevel']])
+            print(data)
 
 
 
